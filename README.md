29cffb1bc83683d0e2bdb88a561c8ff7c901afae
Ali Hasan Habib <alimodffs2@gmail.com>
2025-06-30 12:46:21 +0300

    cheackBoard pattern

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Snake-game-project
 It's about when the snake eats food, it gets bigger each time. The purpose of the game is to reach the highest score you can.
+
+# Plan
+![alt text](/img/image-1.png)
---
+++ play/play.js
@@ -0,0 +1,30 @@
+const container = document.querySelector('.container')
+const rows = 14
+const columns = 14
+const totalboxes = []
+
+for (let i = 0; i < rows * columns; i++) {
+  const newEl = document.createElement('div')
+  newEl.setAttribute('class', 'boxes')
+  container.append(newEl)
+}
+
+//// make a new array that are equal to boxes length to fillter them after
+const boxes = document.querySelectorAll('.boxes')
+let i = 0
+boxes.forEach(() => {
+  totalboxes.push(i)
+  i += 1
+})
+
+console.log(totalboxes)
+
+const blackFilter = totalboxes.filter((nums) => {
+  const row = Math.floor(nums / columns)
+  const col = nums % columns
+  return (row + col) % 2 === 0
+})
+
+blackFilter.forEach((nums) => {
+  boxes[nums].style.backgroundColor = 'black'
+})
